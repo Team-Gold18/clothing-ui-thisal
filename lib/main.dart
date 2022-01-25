6f3c92587cecd8b37b55e51aa248a63c288199cc
Team-Gold18 <gitbatch18mora@outlook.com>
2022-01-25 22:58:05 +0530

    Clothing UI designed using Figma Template

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:clothing_ui/pages/homePage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter 1st project',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
